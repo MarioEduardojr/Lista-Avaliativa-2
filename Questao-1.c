159671e3650627594dcdeb026291859ccc760a7a
MarioEduardojr <134741824+MarioEduardojr@users.noreply.github.com>
2023-11-06 00:26:02 -0300

    Update Questao-1.c

--- Questao-1.c
+++ Questao-1.c
@@ -1,41 +1,40 @@
 #include <stdio.h>
+
 int somaVetores(int valores[], int tamanho) {
     while (tamanho > 1) {
         for (int i = 0; i < tamanho - 1; i++) {
             valores[i] = valores[i] + valores[i + 1];
-        } 
-        tamanho--;  
+        }
+        tamanho--;
         for (int i = 0; i < tamanho; i++) {
             printf("%d", valores[i]);
             if (i < tamanho - 1) {
-                printf(" "); 
+                printf(" ");
             }
         }
         printf("\n");
     }
+    return valores[0]; 
 }
 
 int main() {
-    int conjunto_de_valores[10];
-    int n_elementos = 10;
+    int conjuntodevalores[10];
+    int elementos = 10;
 
-    printf("\n");
-
-    for (int i = 0; i < n_elementos; i++) {
-        scanf("%d", &conjunto_de_valores[i]);
+    for (int i = 0; i < elementos; i++) {
+        scanf("%d", &conjuntodevalores[i]);
     }
 
-   
-    
-    for (int i = 0; i < n_elementos; i++) {
-        printf("%d", conjunto_de_valores[i]);
-        if (i < n_elementos - 1) {
-            printf(" "); 
+    for (int i = 0; i < elementos; i++) {
+        printf("%d", conjuntodevalores[i]);
+        if (i < elementos - 1) {
+            printf(" ");
         }
     }
     printf("\n");
 
-    somaVetores(conjunto_de_valores, n_elementos);
+    int resultado_final = somaVetores(conjuntodevalores, elementos);
+    printf("Resultado Final: %d\n", resultadofinal);
 
     return 0;
-} 
+}
