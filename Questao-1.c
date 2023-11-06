21fb62ac09b45eb0866a94d2177c20915b2e8287
MarioEduardojr <134741824+MarioEduardojr@users.noreply.github.com>
2023-11-06 11:31:59 -0300

    Update Questao-1.c

--- Questao-1.c
+++ Questao-1.c
@@ -16,25 +16,25 @@ int somaVetores(int valores[], int tamanho) {
 }
 
 int main() {
-    int conjuntodevalores[10];
-    int elementos = 10;
+    int conjunto_de_valores[10];
+    int numero_elementos = 10;
 
 
-    for (int i = 0; i < elementos; i++) {
-        scanf("%d", &conjuntodedevalores[i]);
+    for (int i = 0; i < numero_elementos; i++) {
+        scanf("%d", &conjunto_de_valores[i]);
     }
 
    
     
-    for (int i = 0; i < elementos; i++) {
-        printf("%d", conjuntodevalores[i]);
-        if (i < elementos - 1) {
+    for (int i = 0; i < numero_elementos; i++) {
+        printf("%d", conjunto_de_valores[i]);
+        if (i < n_elementos - 1) {
             printf(" "); 
         }
     }
     printf("\n");
 
-    somaVetores(conjuntodevalores, elementos);
+    somaVetores(conjunto_de_valores, numero_elementos);
 
     return 0;
 }
