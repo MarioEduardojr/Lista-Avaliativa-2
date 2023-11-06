12859cd0c9901e0df9d45cc4996ff8e42692e34a
MarioEduardojr <134741824+MarioEduardojr@users.noreply.github.com>
2023-11-06 00:35:17 -0300

    Update Questao-3.c

--- Questao-3.c
+++ Questao-3.c
@@ -1,37 +1,33 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
     int A[4][4], B[4][4], result[4][4];
     char operacao[5];
-    
-    // Leitura da matriz A
    
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 4; j++) {
             scanf("%d", &A[i][j]);
         }
     }
-
-    // Leitura da matriz B
-   
+    
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 4; j++) {
             scanf("%d", &B[i][j]);
         }
     }
     
-    // Leitura da operação
-
+    
     scanf("%s", operacao);
     
-    // Realização da operação e cálculo da matriz resultante
+    
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 4; j++) {
-            if (operacao[0] == 'soma') {
+            if (strcmp(operacao, "soma") == 0) {
                 result[i][j] = A[i][j] + B[i][j];
-            } else if (operacao[0] == 'sub') {
+            } else if (strcmp(operacao, "sub") == 0) {
                 result[i][j] = A[i][j] - B[i][j];
-            } else if (operacao[0] == 'mult') {
+            } else if (strcmp(operacao, "mult") == 0) {
                 result[i][j] = 0;
                 for (int k = 0; k < 4; k++) {
                     result[i][j] += A[i][k] * B[k][j];
@@ -39,9 +35,7 @@ int main() {
             }
         }
     }
-
-    // Exibição da matriz resultante formatada
-  
+    
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 4; j++) {
             printf("%4d", result[i][j]);
